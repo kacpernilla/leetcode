f4a8d786eb1352080a9e821201bebd872c7bf36f
kacper <kacper@macbook-air-kacper.tail25b750.ts.net>
2026-09-16 01:12:07 +0200

    #14 Longest Common Prefix

--- main.cpp
+++ main.cpp
@@ -1,50 +1,29 @@
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int fetch(char c){
-    switch(c) {
-        case 'I':
-            return 1;
-            break;
-        case 'V':
-            return 5;
-            break;
-        case 'X':
-            return 10;
-            break;
-        case 'L':
-            return 50;
-            break;
-        case 'C':
-            return 100;
-            break;
-        case 'D':
-            return 500;
-            break;
-        case 'M':
-            return 1000;
-            break;
-
-        default:
-            break;
-
-    }
-    return 0;
-}
-
 class Solution {
 public:
-    int romanToInt(string s) {
-        int total = 0;
+    string longestCommonPrefix(vector<string>& strs) {
+        int total = strs[0].length();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i + 1 < s.length() && fetch(s[i]) < fetch(s[i + 1]))
-                total -= fetch(s[i]);
-            else
-                total += fetch(s[i]);
+        for(int i = 0; i < strs.size() - 1; i++){
+            int current = 0;
+
+            for(int j = 0; j < strs[i].length() && j < strs[i+1].length(); j++){
+                if(strs[i][j] == strs[i+1][j]){
+                    current += 1;
+                } else {
+                    break;
+                }
+            }
+
+            total = min(total, current);
         }
 
-        return total;
+        return string(strs[0].begin(), strs[0].begin() + total);
     }
 };
+
+int main(){ }
