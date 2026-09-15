2dd5c8b04a661117fc668243e8966409bade7f13
kacper <kacper@macbook-air-kacper.tail25b750.ts.net>
2026-09-15 23:19:11 +0200

    #9 Palindrome Number

--- main.cpp
+++ main.cpp
@@ -6,26 +6,16 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
+    bool isPalindrome(int x) {
+        string classic = to_string(x);
+        string reverse(classic.rbegin(), classic.rend());
 
-        for(size_t i = 0; i < nums.size(); i++){
-            m[nums[i]] += 1;
+        for(size_t i = 0; i < classic.length(); i++){
+            if(classic[i] != reverse[i]){
+                return false;
+            }
         }
-    
-        vector<pair<int, int>> sorted(m.begin(), m.end());
-
-        sort(sorted.begin(), sorted.end(), [](auto& a, auto &b){
-            return a.second > b.second;
-        });
-
-        vector<int> res;
-
-        for(int i = 0; i < k; i++){
-            res.push_back(sorted[i].first);
-        }
-
-        return res;
+        return true;
     }
 };
 
