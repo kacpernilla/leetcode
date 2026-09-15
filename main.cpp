b600de3545eaf3e51d06248f2e91578dea04a63c
kacper <kacper@macbook-air-kacper.tail25b750.ts.net>
2026-09-15 20:30:01 +0200

    #217 Contains Duplicate

--- main.cpp
+++ main.cpp
@@ -4,54 +4,20 @@
 
 using namespace std;
 
-template <typename T>
-void test(const T& actual, const T& expected, const string& name) {
-    if (actual == expected)
-        cout << "[PASS] " << name << '\n';
-    else
-        cout << "[FAIL] " << name << '\n';
-}
-
-template <typename T>
-void testVector(const vector<T>& actual,
-                const vector<T>& expected,
-                const string& name) {
-    if (actual == expected)
-        cout << "[PASS] " << name << '\n';
-    else
-        cout << "[FAIL] " << name << '\n';
-}
-
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        seen.reserve(nums.size());
 
-        unordered_map<char, int> m;
-
-        for(char c : s){ m[c] += 1; }
-
-        for(char c : t){
-            if(--m[c] < 0) return false;
+        for (int x : nums) {
+            if (!seen.insert(x).second) {
+                return true;
+            }
         }
-
-        return true;
+        return false;
     }
 };
 
-int main(void){
-    Solution s;
-
-    test(
-        s.isAnagram("anagram", "nagaram"),
-        true,
-        "1"
-    );
-
-    test(
-        s.isAnagram("rat", "car"),
-        false,
-        "2"
-    );
-}
+int main(void){ }
 
